44e6701df298a30e93b0fc57b48870a3f0a5cde4
BVerstaen <bverstaen04@gmail.com>
2025-12-11 16:03:53 +0100

    [ADD] Lever souns

--- scripts/lever.gd
+++ scripts/lever.gd
@@ -1,6 +1,7 @@
 class_name lever extends Sprite2D
 
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var sound_player : AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 var is_interacted : bool = false
 
@@ -10,6 +11,7 @@ func _on_interactable_player_interact() -> void:
 	on_interacted.emit()
 	#Play corresponding animation
 	is_interacted = !is_interacted
+	sound_player.play()
 	if is_interacted:
 		animation_player.play("Switch")
 	else:
